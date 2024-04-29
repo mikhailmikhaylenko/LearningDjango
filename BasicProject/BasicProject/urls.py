0a77041e85302db7ada6a71309236d2870b4f713
Mikhail Mikhaylenko <auth1d1@gmail.com>
2024-04-29 19:36:35 +0300

    Create Django app-specific page

--- BasicProject/BasicProject/urls.py
+++ BasicProject/BasicProject/urls.py
@@ -20,7 +20,12 @@ Including another URLconf
 #from django.contrib import admin
 #from django.urls import path
 
+from django.urls import include, re_path
+import HelloDjangoApp.views
+
 urlpatterns = [
     # Uncomment the next line to enable the admin:
     #path('admin/', admin.site.urls)
+    re_path(r'^$', HelloDjangoApp.views.index, name='index'),
+    re_path(r'^home$', HelloDjangoApp.views.index, name='home')
 ]
